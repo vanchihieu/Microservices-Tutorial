99478bbfaa423d66fb2b751a0325f97cb4478935
Van Chi Hieu <vanchihieu3@gmail.com>
2024-11-19 23:23:47 +0700

    Authentication và Authorization giữa các micro-service

--- identity-service/src/main/java/com/devteria/identity/repository/httpclient/ProfileClient.java
+++ identity-service/src/main/java/com/devteria/identity/repository/httpclient/ProfileClient.java
@@ -1,5 +1,7 @@
 package com.devteria.identity.repository.httpclient;
 
+import com.devteria.identity.configuration.AuthenticationRequestInterceptor;
+import com.devteria.identity.dto.request.ApiResponse;
 import com.devteria.identity.dto.request.ProfileCreationRequest;
 import com.devteria.identity.dto.response.UserProfileResponse;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -7,8 +9,9 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "profile-service", url = "${app.services.profile}")
+@FeignClient(name = "profile-service", url = "${app.services.profile}",
+        configuration = { AuthenticationRequestInterceptor.class })
 public interface ProfileClient {
     @PostMapping(value = "/internal/users", produces = MediaType.APPLICATION_JSON_VALUE)
-    UserProfileResponse createProfile(@RequestBody ProfileCreationRequest request);
+    ApiResponse<UserProfileResponse> createProfile(@RequestBody ProfileCreationRequest request);
 }
